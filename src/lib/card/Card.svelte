1ed38d66d64c1503ce6e51ec28db79ae22823b1a
codepert <trang.uixdev@gmail.com>
2024-06-10 05:26:21 +0900

    Add card component.

---
+++ src/lib/card/Card.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let title: string | undefined = undefined;
+	export let desc: string | undefined = undefined;
+</script>
+
+<div>
+	<h1>{title}</h1>
+	<p>{desc}</p>
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Card from '$lib/card/Card.svelte';
+</script>
+
+<Card title="title" desc="desc">
+	<small>test svelte card</small>
+</Card>
